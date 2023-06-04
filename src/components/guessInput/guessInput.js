6e086d494341185dcfff21098929a434a891e993
Erika Wiedemann (Burdon) <eburdon@users.noreply.github.com>
2023-06-04 13:06:21 -0700

    Guessinput

---
+++ src/components/guessInput/guessInput.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function GuessInput() {
+  const [localGuess, setLocalGuess] = React.useState('');
+
+  const handleLocalGuess = (letter) => {
+    setLocalGuess(letter)
+  }
+
+  const handleLocalSubmit = () => {
+    console.log("Local Submit", {localGuess})
+
+    if (localGuess.length !== 5) {
+      window.alert("Please use exactly 5 letters")
+      return
+    }
+
+    setLocalGuess('');
+  }
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        handleLocalSubmit();
+      }}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        value={localGuess}
+        maxLength={5}
+        minLength={5}
+        onChange={(event) => {
+          event.preventDefault();
+          handleLocalGuess(event.target.value.toUpperCase())
+        }}
+      />
+    </form >);
+}
+
+export default GuessInput;
